91cf06fa11155cd25fd58ef526fbbbf6fab776bf
Artem <zagwork@gmail.com>
2022-07-13 09:10:15 +0300

    перенос гантта в отедельную папку

--- resources/views/task/task_project_tree.blade.php
+++ resources/views/task/task_project_tree.blade.php
@@ -6,40 +6,40 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Teamwork</title>
 
-    <link rel=stylesheet href="/platform.css" type="text/css">
-    <link rel=stylesheet href="/libs/jquery/dateField/jquery.dateField.css" type="text/css">
+    <link rel=stylesheet href="/gantt/platform.css" type="text/css">
+    <link rel=stylesheet href="/gantt/libs/jquery/dateField/jquery.dateField.css" type="text/css">
 
-    <link rel=stylesheet href="/gantt.css" type="text/css">
-    <link rel=stylesheet href="/gantt_status_colors.css" type="text/css">
-    <link rel=stylesheet href="/ganttPrint.css" type="text/css" media="print">
-    <link rel=stylesheet href="/libs/jquery/valueSlider/mb.slider.css" type="text/css" media="print">
+    <link rel=stylesheet href="/gantt/gantt.css" type="text/css">
+    <link rel=stylesheet href="/gantt/gantt_status_colors.css" type="text/css">
+    <link rel=stylesheet href="/gantt/ganttPrint.css" type="text/css" media="print">
+    <link rel=stylesheet href="/gantt/libs/jquery/valueSlider/mb.slider.css" type="text/css" media="print">
 
-    <script src="/js/jquery_3.1.1.min.js"></script>
-    <script src="/js/jquery-ui.1.12.1.min.js"></script>
+    <script src="/gantt/libs/jquery/jquery_3.1.1.min.js"></script>
+    <script src="/gantt/libs/jquery/jquery-ui.1.12.1.min.js"></script>
 
-    <script src="/libs/jquery/jquery.livequery.1.1.1.min.js"></script>
-    <script src="/libs/jquery/jquery.timers.js"></script>
+    <script src="/gantt/libs/jquery/jquery.livequery.1.1.1.min.js"></script>
+    <script src="/gantt/libs/jquery/jquery.timers.js"></script>
 
-    <script src="/libs/utilities.js"></script>
-    <script src="/libs/forms.js"></script>
-    <script src="/libs/date.js"></script>
-    <script src="/libs/dialogs.js"></script>
-    <script src="/libs/layout.js"></script>
-    <script src="/libs/i18nJs.js"></script>
-    <script src="/libs/jquery/dateField/jquery.dateField.js"></script>
-    <script src="/libs/jquery/JST/jquery.JST.js"></script>
-    <script src="/libs/jquery/valueSlider/jquery.mb.slider.js"></script>
+    <script src="/gantt/libs/utilities.js"></script>
+    <script src="/gantt/libs/forms.js"></script>
+    <script src="/gantt/libs/date.js"></script>
+    <script src="/gantt/libs/dialogs.js"></script>
+    <script src="/gantt/libs/layout.js"></script>
+    <script src="/gantt/libs/i18nJs.js"></script>
+    <script src="/gantt/libs/jquery/dateField/jquery.dateField.js"></script>
+    <script src="/gantt/libs/jquery/JST/jquery.JST.js"></script>
+    <script src="/gantt/libs/jquery/valueSlider/jquery.mb.slider.js"></script>
 
-    <script type="text/javascript" src="/libs/jquery/svg/jquery.svg.min.js"></script>
-    <script type="text/javascript" src="/libs/jquery/svg/jquery.svgdom.1.8.js"></script>
+    <script type="text/javascript" src="/gantt/libs/jquery/svg/jquery.svg.min.js"></script>
+    <script type="text/javascript" src="/gantt/libs/jquery/svg/jquery.svgdom.1.8.js"></script>
 
 
-    <script src="/ganttUtilities.js"></script>
-    <script src="/ganttTask.js"></script>
-    <script src="/ganttDrawerSVG.js"></script>
-    <script src="/ganttZoom.js"></script>
-    <script src="/ganttGridEditor.js"></script>
-    <script src="/ganttMaster.js"></script>
+    <script src="/gantt/ganttUtilities.js"></script>
+    <script src="/gantt/ganttTask.js"></script>
+    <script src="/gantt/ganttDrawerSVG.js"></script>
+    <script src="/gantt/ganttZoom.js"></script>
+    <script src="/gantt/ganttGridEditor.js"></script>
+    <script src="/gantt/ganttMaster.js"></script>
 
 
     <!--<script src="libs/profiling.js"></script>-->
@@ -88,7 +88,7 @@
 
         // here starts gantt initialization
         ge = new GanttMaster();
-        ge.resourceUrl = "/res/";
+        ge.resourceUrl = "/gantt/res/";
         ge.permissions.canSeePopEdit = false;
         ge.set100OnClose = true;
 
