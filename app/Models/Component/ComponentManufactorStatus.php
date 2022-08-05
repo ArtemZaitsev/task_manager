9ee5e15209f19d9ca8ab5cf2ba2e31e394518375
Artem <zagwork@gmail.com>
2022-08-05 11:02:37 +0300

    status order

--- app/Models/Component/ComponentManufactorStatus.php
+++ app/Models/Component/ComponentManufactorStatus.php
@@ -6,18 +6,20 @@ class ComponentManufactorStatus
 {
     public const NOT_REQUIRED = 1;
     public const DOES_NOT_HAVE = 2;
-    public const IN_PROGRESS = 3;
-    public const DONE = 4;
-    public const NOT_DD = 5;
-    public const KTU = 6;
+    public const NOT_DD = 3;
+    public const KTU = 4;
+    public const IN_PROGRESS = 5;
+    public const DONE = 6;
+
 
     public const LABELS = [
         self::NOT_REQUIRED => 'Не требуется',
         self::DOES_NOT_HAVE => 'Нет',
-        self::IN_PROGRESS => 'В процессе',
-        self::DONE => 'Готово',
         self::NOT_DD => 'КД не передано',
         self::KTU => 'Разработка КТУ',
+        self::IN_PROGRESS => 'В процессе',
+        self::DONE => 'Готово',
+
     ];
 
     public static function values(): array
--- app/Models/Component/ComponentPurchaserStatus.php
+++ app/Models/Component/ComponentPurchaserStatus.php
@@ -6,20 +6,18 @@ class ComponentPurchaserStatus
 {
     public const NOT_REQUIRED = 1;
     public const DOES_NOT_DONE = 2;
-    public const IN_PROGRESS = 3;
-    public const DONE = 4;
-    public const COMPONENT_CHOOSED = 5;
-    public const AGREE = 6;
+    public const AGREE = 3;
+    public const COMPONENT_CHOOSED = 4;
+    public const IN_PROGRESS = 5;
+    public const DONE = 6;
 
     public const LABELS = [
-
-
+        self::NOT_REQUIRED => 'Не требуется',
+        self::DOES_NOT_DONE => 'Не проработана',
+        self::AGREE => 'Согласование',
+        self::COMPONENT_CHOOSED => 'Компонент выбран',
         self::IN_PROGRESS => 'В процессе',
         self::DONE => 'Готово',
-        self::DOES_NOT_DONE => 'Не проработана',
-        self::NOT_REQUIRED => 'Не требуется',
-        self::COMPONENT_CHOOSED => 'Компонент выбран',
-        self::AGREE => 'Согласование',
     ];
 
     public static function values(): array
