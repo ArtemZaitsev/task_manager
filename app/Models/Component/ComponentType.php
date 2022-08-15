fa214b82e64bc9a8a1fc087dc81d76a72545da43
Artem <zagwork@gmail.com>
2022-08-15 16:37:54 +0300

    импорт прочие изделия

--- app/Models/Component/ComponentType.php
+++ app/Models/Component/ComponentType.php
@@ -8,12 +8,14 @@ class ComponentType
     public const DETAIL = 2;
     public const PURCHASED_ITEM = 3;
     public const STANDART_PRODUCT = 4;
+    public const OTHER_PRODUCT = 5;
 
     public const LABELS = [
         self::ASSEMBLY_UNIT => 'Сборочная единица',
         self::DETAIL => 'Деталь',
         self::PURCHASED_ITEM => 'Покупное изделие',
         self::STANDART_PRODUCT => 'Стандартное изделие',
+        self::OTHER_PRODUCT => 'Прочие изделия',
     ];
 
     public static function values(): array
